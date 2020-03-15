1585b00838bff87786f69f250522c8ca2f08f236
Benjamin Rowe <browe495@gmail.com>
2020-03-15 16:01:15 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,15 @@
 # Add your variables here
+#first_number = get_variable_from_file('./calculator.rb', "first_number")
+
+#second_number = get_variable_from_file('./calculator.rb', "second_number")
+
+#def add('first_number', 'second_number')
+  sum = first_number + second_number
+  p sum
+#end
+
+#difference = first_number - second_number
+
+#product = first_number * second_number
+
+#quotient = first_number / second_number
