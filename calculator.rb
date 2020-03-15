58b862546d792377d7ffccb2a1e4bb4035d9fa51
Benjamin Rowe <browe495@gmail.com>
2020-03-15 16:11:54 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 # Add your variables here
 
-first_number = Integer or Float
-second_number = Integer or Float
-second_number != 0
+first_number = 1
+second_number = 2
+#second_number != 0
 sum = first_number = second_number
