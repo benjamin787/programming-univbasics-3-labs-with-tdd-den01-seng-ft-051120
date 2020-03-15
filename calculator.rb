d9379a8d6e4f3029a17b675a79802db860f9bade
Benjamin Rowe <browe495@gmail.com>
2020-03-15 16:10:46 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,16 +2,5 @@
 
 first_number = Integer or Float
 second_number = Integer or Float
-
-#def calculator(first_number, second_number)
-  p first_number
-  p second_number
-end
-
-#def sum
-  calculator
-  sum = first_number + second_number
-  p sum
-end
-
-#sum
+second_number != 0
+sum = first_number = second_number
